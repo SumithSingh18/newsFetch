731b573ecf6e2e688e93fa9df636024086f51901
Sumith Singh <ss.sumi1804@gmail.com>
2025-05-06 18:31:18 +0530

    changes made to stop_container

--- stop_container.sh
+++ stop_container.sh
@@ -2,3 +2,8 @@ set -e
 
 echo "Hi, stopping the container..."
 # Stop the container
+
+containerid=`docker ps | awk -F " " '{print $1}'`
+docker rm -f $containerid
+echo "Container stopped successfully."
+
